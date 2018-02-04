f85433bf3ebaab3f9997b0400e9a6f901aed8a50
NikVolf <nikvolf@gmail.com>
2018-02-04 20:01:20 +0300

    new wasmi interpreter, runtime refactor

--- ethcore/vm/src/schedule.rs
+++ ethcore/vm/src/schedule.rs
@@ -127,20 +127,14 @@ pub struct WasmCosts {
 	pub mul: u32,
 	/// Memory (load/store) operations multiplier.
 	pub mem: u32,
-	/// Memory copy operation, per byte.
-	pub mem_cmp: u32,
-	/// Memory copy operation, per byte.
-	pub mem_copy: u32,
-	/// Memory move operation, per byte.
-	pub mem_move: u32,
-	/// Memory set operation, per byte.
-	pub mem_set: u32,
 	/// Static region charge, per byte.
 	pub static_region: u32,
 	/// General static query of U256 value from env-info
 	pub static_u256: u32,
 	/// General static query of Address value from env-info
 	pub static_address: u32,
+	/// Memory stipend. Amount of free memory (in 64kb pages) each contract can use for stack.
+	pub memory_stipend: u32,
 }
 
 impl Default for WasmCosts {
@@ -150,13 +144,10 @@ impl Default for WasmCosts {
 			div: 16,
 			mul: 4,
 			mem: 2,
-			mem_cmp: 1,
-			mem_copy: 1,
-			mem_move: 1,
-			mem_set: 1,
 			static_region: 1,
 			static_u256: 64,
 			static_address: 40,
+			memory_stipend: 17,
 		}
 	}
 }
